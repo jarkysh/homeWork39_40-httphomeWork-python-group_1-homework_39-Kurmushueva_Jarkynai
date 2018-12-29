e9b309789e3b2ab9b4dfb4a7356ab9471d7309c6
Jarkynai <jarkynai.kurmushueva@gmail.com>
2018-12-29 17:28:17 +0600

    Доавление представления для создание задачи

---
+++ todo_list/webapp/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from webapp.models import Task
+from django.shortcuts import render
+
+
+def index_view(request):
+    tasks = Task.objects.all()
+    return render(request, 'index.html', context={
+        'tasks': tasks
+    })
+
+
+def create_view(request):
+    if request.method == 'GET':
+        return render(request, 'create.html')
+    elif request.method == 'POST':
+        Task.objects.create(
+            name=request.POST.get('name'),
+            due_date=request.POST.get('due_date')
+    )
+    return redirect('index')
+
+
+def delete_view(request, task_pk):
+    task = get_object_or_404(Task, pk=task_pk)
+
+    if request.method == 'GET':
+        return render(request, 'delete.html', context={
+            'task': task
+        })
+    elif request.method == 'POST':
+        if request.POST.get('delete') == 'yes':
+            task.delete()
+        return redirect('index')
+
+def edit_view(request,task_pk):
+    task = get_object_or_404(Task, pk=task_pk)
+
+    if request.method == 'GET':
+        context = {'task': task}
+        return render(request, update.html, context={
+            'task': task
+        })
+    elif request.method == 'POST'
+        task.name = request.POST.get('name')
+        task.description = request.POST.get('text')
+        task.status = request.POST.get.('status')
+        task.created_at = request.POST.get('due_date')
+        task.save()
+        return redirect('index')
